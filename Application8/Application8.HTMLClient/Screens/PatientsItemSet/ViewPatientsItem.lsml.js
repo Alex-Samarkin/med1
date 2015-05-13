d71d5fbfe9dfc85d05975560c58e2171b1972750
alex <alex_samarkin@mail.ru>
2015-05-14 02:03:46 +0300

    m2

---
+++ Application8/Application8.HTMLClient/Screens/PatientsItemSet/ViewPatientsItem.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewPatientsItem.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.PatientsItem.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.PatientsItem." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
---
+++ Application8/Application8.HTMLClient/Screens/ShedulesItemSet/ViewShedulesItem.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewShedulesItem.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.ShedulesItem.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.ShedulesItem." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
