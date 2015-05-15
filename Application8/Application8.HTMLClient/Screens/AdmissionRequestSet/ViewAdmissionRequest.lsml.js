af802767f966f90dda1f62d8f08a782c5735f915
alex <alex_samarkin@mail.ru>
2015-05-15 03:25:55 +0300

    добавлены заявки на прием

---
+++ Application8/Application8.HTMLClient/Screens/AdmissionRequestSet/ViewAdmissionRequest.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewAdmissionRequest.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.AdmissionRequest.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.AdmissionRequest." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
---
+++ Application8/Application8.HTMLClient/Screens/ProfessionListSet/ViewProfessionList.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewProfessionList.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.ProfessionList.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.ProfessionList." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
