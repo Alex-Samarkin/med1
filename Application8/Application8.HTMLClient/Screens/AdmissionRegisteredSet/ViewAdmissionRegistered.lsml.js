364f0740afa06c1e4139feba6b27d2bfaf2a6b02
alex <alex_samarkin@mail.ru>
2015-05-17 23:35:57 +0300

    поправлены экраны поиска

---
+++ Application8/Application8.HTMLClient/Screens/AdmissionRegisteredSet/ViewAdmissionRegistered.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewAdmissionRegistered.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.AdmissionRegistered.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.AdmissionRegistered." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
